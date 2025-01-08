c62e2216497321ebcde5d8444512bf5c8854967a
Pranshu Patel <pranshu05patel@gmail.com>
2025-01-08 23:16:50 +0530

    Refactor StarryBackground component to enhance twinkling effect and increase star count

--- src/components/StarryBackground.jsx
+++ src/components/StarryBackground.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useRef } from "react";
 
 const StarryBackground = () => {
@@ -7,32 +7,29 @@ const StarryBackground = () => {
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
-        const stars = Array(500).fill().map(() => ({
+        const stars = Array(750).fill().map(() => ({
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
-            radius: Math.random() * 1.5,
+            radius: Math.random() * 1,
             alpha: Math.random() * 0.5 + 0.5,
-            velocity: Math.random() * 0.2 + 0.1,
+            alphaChange: Math.random() * 0.02 + 0.01, 
         }));
 
         function drawStars() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
             stars.forEach((star) => {
+                star.alpha += star.alphaChange;
+                if (star.alpha <= 0.5 || star.alpha >= 1) {
+                    star.alphaChange *= -1;
+                }
                 ctx.beginPath();
                 ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
                 ctx.fillStyle = `rgba(255, 255, 255, ${star.alpha})`;
                 ctx.fill();
-
-                // Twinkle effect
-                star.alpha += star.velocity;
-                if (star.alpha >= 1 || star.alpha <= 0.5) {
-                    star.velocity *= -1;
-                }
             });
 
             requestAnimationFrame(drawStars);
